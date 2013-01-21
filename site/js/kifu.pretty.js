eb1d7a59bb50b549afb0ecaf4fbe3899c82d80bc
Frank Lam <frank@almostflan.com>
2013-01-21 00:46:24 -0800

    lesson 3

--- site/js/kifu.pretty.js
+++ site/js/kifu.pretty.js
@@ -293,6 +293,9 @@ Record.prototype.loadFromSgfString = function(sgf_data) {
                     cur_mv.lb.push(value);
                 } else if (method == "TR") {
                     cur_mv.tr.push(value);
+                } else if (method == "SZ") {
+                    this.board.size = parseInt(value);
+                    this.board.clearBoard()
                 }
             }
         } else {
@@ -482,34 +485,36 @@ function sgfCoordToIndecies(sgf_coord) {
     }
 }
 function drawBoard(board, canvas) {
-    var ctx = canvas.getContext("2d"), dim = 450, margins = 10,
+    var ctx = canvas.getContext("2d"), dim = 450, margins = 30,
         i, j, dx, dy, x_pos, y_pos, stone, black_stone, stone_radius, padded_stone_radius;
 
     ctx.clearRect(0,0,500,500);
     ctx.beginPath();
 
     // Vertical lines
-    for (i = 0; i <= 18; i++) {
-        dx = i/18*dim + margins;
+    for (i = 0; i <= board.size - 1; i++) {
+        dx = i/(board.size - 1)*dim + margins;
         ctx.moveTo(dx, margins);
         ctx.lineTo(dx, dim + margins);
     }
 
     // Horizontal lines
-    for (i = 0; i <= 18; i++) {
-        dy = i/18*dim + margins;
+    for (i = 0; i <= (board.size - 1); i++) {
+        dy = i/(board.size - 1)*dim + margins;
         ctx.moveTo(margins, dy);
         ctx.lineTo(dim + margins, dy);
     }
 
     // Star points
-    ctx.fillStyle = "rgb(0,0,0)";
-    for (i = 3; i <= 15; i++) {
-        for (j = 3; j <= 15; j++) {
-            if (i % 6 == 3 && j % 6 == 3) {
-                dx = i/18*dim + margins;
-                dy = j/18*dim + margins;
-                ctx.fillRect(dx - 3, dy - 3, 6, 6);
+    if (board.size == 19) {
+        ctx.fillStyle = "rgb(0,0,0)";
+        for (i = 3; i <= 15; i++) {
+            for (j = 3; j <= 15; j++) {
+                if (i % 6 == 3 && j % 6 == 3) {
+                    dx = i/(board.size - 1)*dim + margins;
+                    dy = j/(board.size - 1)*dim + margins;
+                    ctx.fillRect(dx - 3, dy - 3, 6, 6);
+                }
             }
         }
     }
@@ -524,9 +529,9 @@ function drawBoard(board, canvas) {
             stone = board.stones[i][j];
             annotation = board.annotations[i][j];
 
-            x_pos = i / 18 * dim + margins;
-            y_pos = j / 18 * dim + margins;
-            stone_radius = dim / (18 * 3);
+            x_pos = i / (board.size - 1) * dim + margins;
+            y_pos = j / (board.size - 1) * dim + margins;
+            stone_radius = dim / ((board.size - 1) * 3);
 
             if (stone) {
                 ctx.beginPath();
@@ -545,7 +550,7 @@ function drawBoard(board, canvas) {
             black_stone = stone && stone.color == "b";
             if (annotation) {
                 if (annotation == "[tr]") {
-                    padded_stone_radius = stone_radius + 2;
+                    padded_stone_radius = stone_radius - 1;
                     ctx.beginPath();
                     ctx.moveTo(x_pos, y_pos - padded_stone_radius);
                     ctx.lineTo(x_pos + padded_stone_radius * Math.sqrt(3) / 2, y_pos + padded_stone_radius/2);
@@ -561,7 +566,7 @@ function drawBoard(board, canvas) {
                 } else {
                     if (!black_stone) {
                         ctx.beginPath();
-                        ctx.arc(x_pos, y_pos, stone_radius, 0, 2 * Math.PI);
+                        ctx.arc(x_pos, y_pos, stone_radius - 1, 0, 2 * Math.PI);
                         ctx.strokeStyle = "rgb(255,255,255)";
                         ctx.stroke();
                         ctx.fillStyle = "rgb(255,255,255)";
